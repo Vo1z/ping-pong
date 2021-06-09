543514db6bcf601f678c35b517c1845b1e631501
Vo1z <vitya.voody@gmail.com>
2021-06-09 02:50:20 +0200

    Dev 4
    -TextMeshPro was reimported due to importing bug
    -ManuManager script was replaced with UiManager
    -Main menu was implemented
    -Ingame menu was implemented

---
+++ Assets/Scripts/UI/UiManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TestTask.UI
+{
+    public class UiManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject hud;
+        [SerializeField] private GameObject pauseMenu;
+        [Space]
+        [SerializeField] private TextMeshProUGUI levelLabel;
+        
+        private void Start()
+        {
+            if(GameManager.Instance != null)
+                GameManager.Instance.OnLevelUp += UpdateLevelLabel;
+            UpdateLevelLabel();
+            
+            if(hud != null)
+                hud.SetActive(true);
+            if(pauseMenu != null)
+                pauseMenu.SetActive(false);
+        }
+
+        public void StartNewGame() => SceneManager.LoadScene(1);
+        
+        public void InvokeGameMenu()
+        {
+            if(hud != null)
+                hud.SetActive(false);
+            if(pauseMenu != null)
+                pauseMenu.SetActive(true);
+            
+            Time.timeScale = 0;
+        }
+
+        public void InvokeHUD()
+        {
+            if(pauseMenu != null)
+                pauseMenu.SetActive(false);
+            if(hud != null)
+                hud.SetActive(true);
+
+            Time.timeScale = 1;
+        }
+
+        public void ExitGame() => Application.Quit();
+
+        private void UpdateLevelLabel()
+        {
+            if(levelLabel != null)
+                levelLabel.text = $"{GameManager.Instance.CurrentLevel}";
+        }
+    }
+}
